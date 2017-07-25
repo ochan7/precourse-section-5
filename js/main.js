977461fd4d2c2a735a8db4b71c5cf047ecd44f47
Olie Chan <olie.chan.141@gmail.com>
2017-07-25 23:03:28 +0100

    made a fucntion to create a list of alphabet buttons

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+var lettersArray = letters.split('');
+var $alphabet = document.getElementsByClassName("alphabet");
+function myFunction(){
+  for(var i =0; i<lettersArray.length; i++){
+  var btn = document.createElement("BUTTON");
+  var t = document.createTextNode(lettersArray[i]);
+  btn.appendChild(t);
+  document.body.appendChild(btn);
+    console.log($alphabet);
+  };
+};
+myFunction();
+$('button').hover(function(){
+  $(this).css("background-color","lightgrey");
+  
+},
+function(){
+  $(this).css("background-color","white");
+});
+
+
+});
